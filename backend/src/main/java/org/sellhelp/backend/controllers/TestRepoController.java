1adda650c7c2c50ce46eff2c158f71a656fe1ae2
MetzRoland <metzroland1111@gmail.com>
2025-11-22 14:20:16 +0100

    SecurityConfig added with CORS configuration

--- backend/src/main/java/org/sellhelp/backend/controllers/TestRepoController.java
+++ backend/src/main/java/org/sellhelp/backend/controllers/TestRepoController.java
@@ -7,12 +7,14 @@ import org.sellhelp.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/public")
 public class TestRepoController {
     private final CityRepository cityRepository;
     private final UserRepository userRepository;
