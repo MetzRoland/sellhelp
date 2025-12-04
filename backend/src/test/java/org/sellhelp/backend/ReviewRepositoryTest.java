156f4711977863817ff7a4e7f38996f66fa8ff88
Jani1XY <omboli.janos.w@gmail.com>
2025-12-04 22:35:50 +0100

    Update ReviewRepositoryTest

--- backend/src/test/java/org/sellhelp/backend/ReviewRepositoryTest.java
+++ backend/src/test/java/org/sellhelp/backend/ReviewRepositoryTest.java
@@ -1,8 +1,5 @@
 package org.sellhelp.backend;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,36 +7,42 @@ import org.junit.jupiter.api.Test;
 import org.sellhelp.backend.entities.Review;
 import org.sellhelp.backend.entities.User;
 import org.sellhelp.backend.repositories.ReviewRepository;
+import org.sellhelp.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @DataJpaTest
-public class ReviewRepositoryTest {
+class ReviewRepositoryTest {
 
     @Autowired
     private ReviewRepository reviewRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     private User testSenderUser;
     private User testReviewedUser;
     private Review testReview;
 
     @BeforeEach
-    public void init() {
-        testSenderUser = User.builder()
+    void init() {
+        testSenderUser = userRepository.save(User.builder()
                 .username("NagyB")
                 .firstName("Kis")
                 .lastName("Béla")
-                .birthDate(LocalDate.of(2001, 11, 03))
+                .birthDate(LocalDate.of(2001, 11, 3))
                 .email("kB@newMail.mail")
-                .build();
+                .build());
 
-        testReviewedUser = User.builder()
+        testReviewedUser = userRepository.save(User.builder()
                 .username("metzroland")
                 .firstName("Roland")
                 .lastName("Metz")
                 .birthDate(LocalDate.of(2003, 5, 12))
                 .email("a@gmail.com")
-                .build();
+                .build());
 
         testReview = Review.builder()
                 .senderUser(testSenderUser)
@@ -50,33 +53,51 @@ public class ReviewRepositoryTest {
     }
 
     @Test
-    public void reviewCanBeAddedToReviewRepositoryAndDB() {
+    void reviewCanBeSavedToReviewRepositoryAndDB() {
         Review savedReview = reviewRepository.save(testReview);
-        User senderUserCopy = savedReview.getSenderUser().toBuilder()
-                .id(savedReview.getSenderUser().getId())
-                .createdAt(savedReview.getSenderUser().getCreatedAt())
-                .build();
-
-        User reviewedUserCopy = savedReview.getReviewedUser().toBuilder()
-                .id(savedReview.getReviewedUser().getId())
-                .createdAt(savedReview.getReviewedUser().getCreatedAt())
-                .build();
-
-        Review reviewCopy = savedReview.toBuilder()
-                .id(savedReview.getId())
-                .senderUser(senderUserCopy)
-                .reviewedUser(reviewedUserCopy)
-                .createdAt(savedReview.getCreatedAt())
-                .build();
 
         assertNotNull(savedReview.getId());
-
-        assertEquals(senderUserCopy, savedReview.getSenderUser());
-
-        assertEquals(reviewedUserCopy, savedReview.getReviewedUser());
-
-        assertEquals(reviewCopy, savedReview);
-
+        assertEquals(testSenderUser.getId(), savedReview.getSenderUser().getId());
+        assertEquals(testReviewedUser.getId(), savedReview.getReviewedUser().getId());
+        assertEquals(testReviewedUser.getCreatedAt(), savedReview.getReviewedUser().getCreatedAt());
+        assertEquals("Nagyon segítőkész!", savedReview.getComment());
+        assertEquals(5, savedReview.getRating());
     }
 
+    @Test
+    void reviewCanBeUpdatedToReviewRepositoryAndDB() {
+        Review savedReview = reviewRepository.save(testReview);
+
+        savedReview.setComment("Update");
+        savedReview.setRating((byte) 4);
+        Review updated = reviewRepository.save(savedReview);
+
+        assertEquals(savedReview.getId(), updated.getId());
+        assertEquals("Update", updated.getComment());
+        assertEquals((byte) 4, updated.getRating());
+    }
+
+
+
+    @Test
+    public void reviewGeneralCRUDFunctionalityTest()
+    {
+        Review savedReview = reviewRepository.save(testReview);
+
+        String originalComment = savedReview.getComment();
+        byte originalRating = savedReview.getRating();
+
+        savedReview.setComment("Updated comment");
+        savedReview.setRating((byte) 3);
+        Review updatedReview = reviewRepository.save(savedReview);
+
+        assertNotEquals(originalComment, updatedReview.getComment());
+        assertNotEquals(originalRating, updatedReview.getRating());
+
+        Review test = savedReview.toBuilder().build();
+        assertEquals(test, updatedReview);
+
+        test.setComment("000");
+        assertNotEquals(test, updatedReview);
+    }
 }
