15a2397d67343cf16b25e1a7d3acc49619d240d3
MetzRoland <metzroland1111@gmail.com>
2025-12-09 11:28:40 +0100

    database sql commands separated into multiple sql files

---
+++ data/insert.sql
@@ -0,0 +1,42 @@
+INSERT INTO "user_roles" ("id", "role_name")
+VALUES 
+(1, 'admin'),
+(2, 'moderator'),
+(3, 'user');
+
+INSERT INTO "post_status" ("id", "status_name")
+VALUES 
+(1, 'new'),
+(2, 'closed'),
+(3, 'pending_closure'),
+(4, 'available'),
+(5, 'ended_with_conflict');
+
+INSERT INTO "report_types" ("id", "name")
+VALUES 
+(1, 'scammer'),
+(2, 'dangerous'),
+(3, 'illegal_activity'),
+(4, 'leaked_sensitive_data'),
+(5, 'spam'),
+(6, 'bot/not_a_real_person');
+
+INSERT INTO "counties" ("county_name")
+VALUES 
+('Baranya');
+
+INSERT INTO "cities" ("county_id", "city_name")
+VALUES 
+(1, 'Pécs');
+
+INSERT INTO "users" (
+  "username", "first_name", "last_name", "birth_date",
+  "email", "city_id", "role_id"
+)
+VALUES (
+  'johndoe', 'John', 'Doe', '1990-05-10',
+  'john@example.com', 1, 3
+);
+
+INSERT INTO "user_secrets" ("user_id", "password")
+VALUES (1, 'new_password');
