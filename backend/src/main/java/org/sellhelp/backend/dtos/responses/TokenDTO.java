87330925a5801a9a5e7a1e60f2427d551a1fb25b
Jani1XY <omboli.janos.w@gmail.com>
2025-12-12 22:47:45 +0100

    Add response DTOs

---
+++ backend/src/main/java/org/sellhelp/backend/dtos/responses/TokenDTO.java
@@ -0,0 +1,12 @@
+package org.sellhelp.backend.dtos.responses;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDTO {
+    private String token;
+}
---
+++ backend/src/main/java/org/sellhelp/backend/dtos/responses/UserDTO.java
@@ -0,0 +1,28 @@
+package org.sellhelp.backend.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sellhelp.backend.entities.Review;
+import org.sellhelp.backend.entities.UserFile;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate birthDate;
+    private String cityName;
+    private Instant createdAt;
+    private List<Review> reviews;
+    private List<Review> sentReviews;
+    private List<UserFile> userFiles;
+}
