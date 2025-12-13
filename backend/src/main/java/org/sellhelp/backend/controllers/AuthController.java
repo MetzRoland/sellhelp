997d4d241e80da974724c5f74638137e3a6bf9ef
MetzRoland <metzroland1111@gmail.com>
2025-12-13 11:14:42 +0100

    access and refresh token cookies added

--- backend/src/main/java/org/sellhelp/backend/controllers/AuthController.java
+++ backend/src/main/java/org/sellhelp/backend/controllers/AuthController.java
@@ -1,5 +1,7 @@
 package org.sellhelp.backend.controllers;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.sellhelp.backend.dtos.requests.LoginDTO;
 import org.sellhelp.backend.dtos.requests.RefreshDTO;
@@ -41,14 +43,56 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenDTO> loginHandler(@RequestBody LoginDTO loginDTO)
+    public ResponseEntity<TokenDTO> loginHandler(@RequestBody LoginDTO loginDTO, HttpServletResponse response)
     {
-        return ResponseEntity.ok(authService.loginHandler(loginDTO));
+        TokenDTO tokenDTO = authService.loginHandler(loginDTO);
+
+        Cookie accessTokenCookie = new Cookie("accessToken", tokenDTO.getAccess_token());
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setSecure(false);
+        accessTokenCookie.setPath("/");
+        accessTokenCookie.setMaxAge(15 * 60);
+        accessTokenCookie.setAttribute("SameSite", "Strict");
+
+        Cookie refreshTokenCookie = new Cookie("refreshToken", tokenDTO.getRefresh_token());
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(false);
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setMaxAge(15 * 60);
+        refreshTokenCookie.setAttribute("SameSite", "Strict");
+
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
+
+        return ResponseEntity.ok(tokenDTO);
     }
 
     @PostMapping("/login/refresh")
-    public ResponseEntity<TokenDTO> loginHandler(@RequestBody RefreshDTO refreshDTO)
+    public ResponseEntity<TokenDTO> refreshHandler(@RequestBody RefreshDTO refreshDTO)
     {
         return ResponseEntity.ok(authService.refresh(refreshDTO));
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<String> logoutHandler(@RequestBody RefreshDTO refreshDTO, HttpServletResponse response)
+    {
+        Cookie accessTokenCookie = new Cookie("accessToken", null);
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setSecure(false);
+        accessTokenCookie.setPath("/");
+        accessTokenCookie.setMaxAge(0);
+        accessTokenCookie.setAttribute("SameSite", "Strict");
+
+        Cookie refreshTokenCookie = new Cookie("refreshToken", null);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(false);
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setMaxAge(0);
+        refreshTokenCookie.setAttribute("SameSite", "Strict");
+
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
+
+        return ResponseEntity.ok("Sikeres kijelentkezés!");
     }
 }
