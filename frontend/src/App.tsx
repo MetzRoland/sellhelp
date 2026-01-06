37e12db9d2aa5f067078a0d51175f35684cc060b
MetzRoland <metzroland1111@gmail.com>
2026-01-06 11:54:08 +0100

    Header added to react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,13 @@
-import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router';
-import axios from 'axios';
+import Bejelentkezettfejlc from './components/bejelentkezettfejlc';
 import './App.css';
 
-import TestComponent from './TestComponent';
-
-interface City {
-  id: number;
-  cityName: string;
-}
-
 function App() {
-  const [cities, setCities] = useState<City[]>([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:8080/api/public/getcities")
-      .then((response) => {
-        setCities(response.data);
-        console.log(response.data);
-      });
-  }, []);
 
   return (
     <>
-      <h1>Hello world!</h1>
-
-      {cities.map((city) => {
-        return <p key={city.id}>{city.cityName}</p>
-      })}
-
       <Routes>
-        <Route path='/hi' element={<TestComponent />} />
+        <Route path='/register' element={<Bejelentkezettfejlc />} />
       </Routes>
     </>
   );
