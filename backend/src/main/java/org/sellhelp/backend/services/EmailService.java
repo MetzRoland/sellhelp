b9538c659b61aed551a596076dffcb4f71dcadd1
Jani1XY <omboli.janos.w@gmail.com>
2026-01-18 20:50:45 +0100

    Make EmailService async

--- backend/src/main/java/org/sellhelp/backend/services/EmailService.java
+++ backend/src/main/java/org/sellhelp/backend/services/EmailService.java
@@ -4,11 +4,14 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.sellhelp.backend.exceptions.EmailException;
 import org.sellhelp.backend.security.JWTUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -18,6 +21,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
+@Async("emailExecutor")
 @Service
 public class EmailService {
     private final JavaMailSender javaMailSender;
