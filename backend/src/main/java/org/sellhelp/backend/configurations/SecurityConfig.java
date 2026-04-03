953780a01c7bfb19a4b2c6f79279f7353712bdea
Jani1XY <omboli.janos.w@gmail.com>
2026-04-04 01:02:04 +0200

    Update security config with domain

--- backend/src/main/java/org/sellhelp/backend/configurations/SecurityConfig.java
+++ backend/src/main/java/org/sellhelp/backend/configurations/SecurityConfig.java
@@ -147,7 +147,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("https://*.sellhelp.org", "http://*.sellhelp.org","http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:5173",
+                "http://www.sellhelp.org",
+                "http://sellhelp.org",
+                "https://www.sellhelp.org",
+                "https://sellhelp.org",
+                "https://accounts.google.com"
+                ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
